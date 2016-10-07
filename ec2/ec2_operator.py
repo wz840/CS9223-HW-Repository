a9ff6084917403688a3b2c912139bb7e280d31bf
wz840 <wz840@nyu.edu>
2016-10-06 23:14:01 -0400

    * typo of s3 upload function fixed *

--- ec2/ec2_operator.py
+++ ec2/ec2_operator.py
@@ -81,8 +81,8 @@ for region in boto.ec2.regions():
     bucket = conn.get_bucket(sys.argv[1])
     file = '/home/ec2-user/ec2_status_logging.log'
     key = boto.s3.key.Key(bucket, file)
-    with oper(file) as f:
-      key.send_file(f)
+    with open(file) as f:
+      key.set_contents_from_file(f)
 
   # most likely will get exception on new beta region and gov cloud
   except Exception as e:
